0c1c59703a626829e7dcc75733168bec30f16722
Derrick Jones <derrickjones323@gmail.com>
2012-05-12 20:02:43 -0700

    subscribe to battery state/level notifications

--- BatteryLevelLogger/DJViewController.m
+++ BatteryLevelLogger/DJViewController.m
@@ -18,6 +18,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [[UIDevice currentDevice] setBatteryMonitoringEnabled:YES];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(batterStateDidChange:) name:UIDeviceBatteryStateDidChangeNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(batteryLevelDidChange:) name:UIDeviceBatteryLevelDidChangeNotification object:nil];
 }
 
 - (void)viewDidUnload
@@ -30,5 +33,15 @@
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
+
+#pragma mark - Battery Level Monitoring
+- (void) batteryStateDidChange:(NSNotification *)notification {
+    NSLog(@"battery changed: %@", notification);
+}
+- (void) batteryLevelDidChange:(NSNotification *)notification {
+    NSLog(@"battery level changed %@", notification);
+}
+
+#pragma mark - CoreData
 
 @end
